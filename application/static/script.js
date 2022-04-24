fe402952bbd00bf088df0cfeaa862d1d7cf1e702
nhattuannguyensjsu <100452313+nhattuannguyensjsu@users.noreply.github.com>
2022-04-23 21:41:25 -0700

    Add files via upload

--- application/static/script.js
+++ application/static/script.js
@@ -2,10 +2,10 @@ var itemList = [];
 
 $(function() {
     // Call Python code to sort when clicked
-    $('#sort-by-price').on('click', function(e) {
+    $('#sort-by-name').on('click', function(e) {
         e.preventDefault()
         $.ajax({
-            url:"/sortbyprice",
+            url:"/sortbyname",
             type:"POST",
             dataType: 'json',
             success: function(response){
@@ -32,7 +32,7 @@ $(function() {
                         </div>
                     `;
                 });
-
+    
                 const buttonList = document.querySelectorAll('.add-button');
 
                 buttonList.forEach((button) => {
@@ -66,7 +66,73 @@ $(function() {
                 }
             });
         return false;
-    });
+    })
+
+    $('#sort-by-price').on('click', function(e) {
+        e.preventDefault()
+        $.ajax({
+            url:"/sortbyprice",
+            type:"POST",
+            dataType: 'json',
+            success: function(response){
+                // Sorted list is returned from the Python code
+                console.log(response);
+                document.getElementById("display-items").innerHTML = "";
+                // Loop through sorted list to display items
+                response['data'].forEach(function(item) {
+                    document.getElementById("display-items").innerHTML += 
+                    `
+                        <div class="card mb-3" style="max-width:100%;">
+                            <div class="row no-gutters">
+                                <div class="col-md-4">
+                                    <img src="${item.image}" class="card-img-top img-responsive" alt="${item.name}">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${item.name}</h5>
+                                        <p>${item.price}</p>
+                                        <button id="${item.id}" class="add-button"> Add </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `;
+                });
+    
+                const buttonList = document.querySelectorAll('.add-button');
+
+                buttonList.forEach((button) => {
+                        button.addEventListener('click', () => {
+                            document.getElementById("cart").hidden = false;
+                            
+                            let itemId = parseInt(button.getAttribute("id"));
+                            let itemPrice = parseFloat(allItems[itemId - 1]['price']);
+                            let itemName = allItems[itemId - 1]['name'];
+
+                            let itemInTable = itemList.find(x => x.id === itemId);
+
+                            if (itemInTable == undefined) {
+                                // Add item to cart
+                                var item_t = {"id": itemId, "name": itemName, "quantity": 1, "price": itemPrice};
+                                itemList.push(item_t);
+                                addNewItem(item_t);
+                            } else {
+                                // Update quantity, price, and total price if item is already in cart (adding the same item multiple times)
+                                itemInTable.quantity += 1;
+                                itemInTable.price = +itemPrice * +itemInTable.quantity;
+                                updateItem(itemInTable);
+
+                            }
+                            console.log(itemList);
+                        });
+                    });
+                },
+                error: function(error) {
+                    console.log("error");
+                }
+            });
+        return false;
+    })
 });
 
 
