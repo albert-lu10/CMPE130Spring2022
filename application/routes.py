55465f4c63e78ea89bf0a538b61b668cb3095a59
nhattuannguyensjsu <100452313+nhattuannguyensjsu@users.noreply.github.com>
2022-05-03 18:43:31 -0700

    Update routes.py

--- application/routes.py
+++ application/routes.py
@@ -1,3 +1,4 @@
+
 from flask import render_template, flash, redirect, session, json, request
 from application import app
 import os
@@ -5,6 +6,9 @@ from application import sorting
 from copy import deepcopy
 
 product_data = None
+@app.route('/checkout',methods=['GET','POST'])
+def checkout():
+    return render_template("checkout.html")
 
 @app.route("/")
 @app.route("/home")
@@ -63,12 +67,99 @@ def quickSortbyPrice(a, low, high):
         quickSortbyPrice(a, p+1, high)
     return (a)
 
-def mergeSortbyName():
-    print("Do Mergesort")
+def merge1(arr, l, m, r):
+    item1=m-l+1
+    n1=str(item1["name"])
+    item2=r-m
+    n2 = str(item2["name"])
+ 
+    L = [0] * (n1)
+    R = [0] * (n2)
+ 
+    for i in range(0, n1):
+        item3=L[i]
+        item3["name"] = arr[l + i]
+ 
+    for j in range(0, n2):
+        item4=R[j]
+        item4["name"] = arr[m + 1 + j]
+ 
+    i = 0     
+    j = 0     
+    k = l     
+ 
+    while i < n1 and j < n2:
+        if item3["name"] <= item4["name"]:
+            arr[k] = item3["name"]
+            i += 1
+        else:
+            arr[k] = item4["name"]
+            j += 1
+        k += 1
+ 
+    while i < n1:
+        arr[k] = item3["name"]
+        i += 1
+        k += 1
+    while j < n2:
+        arr[k] = item4["name"]
+        j += 1
+        k += 1
 
-def mergeSortbyPrice():
-    print("Do Mergesort")
+def mergeSortbyName(arr, l, r):
+    if l < r:
+        m = l+(r-l)//2
+        mergeSortbyName(arr, l, m)
+        mergeSortbyName(arr, m+1, r)
+        merge1(arr, l, m, r)
+    
 
+def merge2(arr, l, m, r):
+    item1=m-l+1
+    n1=item1["price"]
+    item2=r-m
+    n2 = item2["price"]
+ 
+    L = [0] * (n1)
+    R = [0] * (n2)
+ 
+    for i in range(0, n1):
+        item3=L[i]
+        item3["price"] = arr[l + i]
+ 
+    for j in range(0, n2):
+        item4=R[j]
+        item4["price"] = arr[m + 1 + j]
+ 
+    i = 0     
+    j = 0     
+    k = l     
+ 
+    while i < n1 and j < n2:
+        if item3["price"] <= item4["price"]:
+            arr[k] = item3["price"]
+            i += 1
+        else:
+            arr[k] = item4["price"]
+            j += 1
+        k += 1
+ 
+    while i < n1:
+        arr[k] = item3["price"]
+        i += 1
+        k += 1
+    while j < n2:
+        arr[k] = item4["price"]
+        j += 1
+        k += 1
+
+def mergeSortbyPrice(arr, l, r):
+    if l < r:
+        m = l+(r-l)//2
+        mergeSortbyPrice(arr, l, m)
+        mergeSortbyPrice(arr, m+1, r)
+        merge2(arr, l, m, r)
+    
 def insertionSortbyName():
     print("Do Insertion Sort")
 
@@ -79,7 +170,7 @@ def insertionSortbyPrice():
 def sort():
     global product_data
     data = deepcopy(product_data)
-
+    
     sort_type = request.args.get('type')
     sort_by = request.args.get('by')
 
@@ -92,9 +183,9 @@ def sort():
             sorted_data = quickSortbyPrice(data,0,len(data)-1)
     elif sort_type == "mergesort":
         if sort_by == "name":
-            sorted_data = mergeSortbyName(data, 'name')
+            sorted_data = mergeSortbyName(data, 0, len(data) -1)
         elif sort_by == "price":
-            sorted_data = mergeSortbyPrice(data, 'price')
+            sorted_data = mergeSortbyPrice(data, 0, len(data) - 1)
     elif sort_type == "insertionsort":
         if sort_by == "name":
             sorted_data = insertionSortbyName(data, 'name')
